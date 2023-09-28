d8ac8aaeb4458d0510874b448fbb09d50fb0beaa
hamza-ben-jemaa <144786620+hamza-ben-jemaa@users.noreply.github.com>
2023-09-28 15:11:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 #find more emojie here: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="my webpage", page_icon=":tada:", layout="wide")
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 # ---- HEADER SECTION ----
-st.subheader("Hi, I am sevn :wave:")
+st.subheader("La Poste Tunisienne")
 st.title("A Data Analyst From Tunisia")
 st.write("I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings")
-st.write("[Learn More >](http://pythonandvba.com)")
+st.write("[Learn More >](https://www.poste.tn/)")
