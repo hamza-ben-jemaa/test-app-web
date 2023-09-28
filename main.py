530902bf731ecbf0955878749ef55562874444c9
hamza-ben-jemaa <144786620+hamza-ben-jemaa@users.noreply.github.com>
2023-09-28 15:02:15 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+#find more emojie here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="my webpage", page_icon=":tada:", layout="wide")
+
+# ---- HEADER SECTION ----
+st.subheader("Hi, I am sevn :wave:")
+st.title("A Data Analyst From Tunisia")
+st.write("I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings")
+st.write("[Learn More >](http://pythonandvba.com)")
